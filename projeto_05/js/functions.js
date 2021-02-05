adc9310429484e552c869a4d953616b2b66ae925
Marcos Vinicius <viniciusmg99@outlook.com>
2021-02-05 18:55:57 -0300

    comit do sliderDepoimento

--- projeto_05/js/functions.js
+++ projeto_05/js/functions.js
@@ -146,4 +146,42 @@ $(function(){
         return false;
     })
 
+
+    // menu responsivo
+    $('.mobile').click(function(){
+        $(this).find('ul').slideToggle();
+    })
+
+
+    // sistema de dedpoimentos
+
+    var amtDepoimento = $('.depoimento-single p').length;
+    var curIndex = 0;
+
+    iniciarDepoimentos();
+    navegarDepoimentos();
+
+    function iniciarDepoimentos(){
+        $('.depoimento-single p').hide();
+        $('.depoimento-single p').eq(0).show();
+    }
+
+    function navegarDepoimentos(){
+        $('[next]').click(function(){
+            curIndex++;
+            if(curIndex >= amtDepoimento)
+                curIndex = 0;
+                $('.depoimento-single p').hide();
+                $('.depoimento-single p').eq(curIndex).show();
+            
+        })
+
+        $('[prev]').click(function(){
+            curIndex--;
+            if(curIndex < 0)
+                curIndex = amtDepoimento-1;
+                $('.depoimento-single p').hide();
+                $('.depoimento-single p').eq(curIndex).show();
+        })
+    }
 })
